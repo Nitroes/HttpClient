f8393fdc7cb23c0b73edf13793da8bc6703bddcd
Nadpex <1761317479@qq.com>
2020-09-21 10:41:03 +0800

    updata

--- app/src/main/java/com/alicetool/project/httpclient/HttpClient.java
+++ app/src/main/java/com/alicetool/project/httpclient/HttpClient.java
@@ -5,4 +5,8 @@ public class HttpClient {
     public int add(int i,int j){
         return  i+j;
     }
+
+    public interface CallBack {
+        void Request(String data) throws Exception;
+    }
 }
